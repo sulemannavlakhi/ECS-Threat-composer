ebab9b3d8aff3e95a18b01ad9e012e064a2e18e6
Your Name <you@example.com>
2026-03-12 16:48:53 -0400

    configuring root provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,14 +7,14 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "suly-S3"           
+    bucket         = "ECS-S3"           
     key            = "terraform.tfstate" 
-    region         = "us-east-1"
+    region         = "ca-central-1"
     use_lockfile   = true   
     encrypt        = true
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "ca-central-1"
 }
