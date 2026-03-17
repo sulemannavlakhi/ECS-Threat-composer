bd61f5cf5692dbbff2f231be11f9e4cf8c31af20
Your Name <you@example.com>
2026-03-17 03:49:38 -0400

    adding force delete to ecr main.tf

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,5 +1,6 @@
 resource "aws_ecr_repository" "ecs_threat" {
   name                 = "threat-composer"
+  force_delete         = true
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
